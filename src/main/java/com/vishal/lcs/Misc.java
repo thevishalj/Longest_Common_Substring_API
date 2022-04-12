893fe15e274e7d0f455a912639bf87da02832006
Vishal Jain <vishalj.svp2000@gmail.com>
2022-04-12 17:58:51 +0530

    Updated Misc.java

--- src/main/java/com/vishal/lcs/Misc.java
+++ src/main/java/com/vishal/lcs/Misc.java
@@ -6,48 +6,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Misc
 {
-    @GetMapping
-    public String hi()
-    {
-        return"Hi from IntelliJ";
-    }
-
-    //Differentwaytotakeinputfromclient
+//    @GetMapping
+//    public String hi()
+//    {
+//        return"Hi from IntelliJ";
+//    }
+//
+//    Differentwaytotakeinputfromclient
 //    @GetMapping(value="input1")
 //    publicStringinput1(HttpServletRequestreq)
 //    {
-//        intn=Integer.parseInt(req.getParameter("n"));//Call->localhost:8080/home?n=10
+//        intn=Integer.parseInt(req.getParameter("n"));         //Call->localhost:8080/home?n=10
 //        return"ThisishomeandvalueEnteredis-"+n;
 //    }
 //
-//@PostMapping("input2")
-//publicvoidinput2(intn,intm,into,HttpSessionsession)
-//        {
-//        intz=n+m+o;//won'tworkifanyvariableismissing
-//        }
+//    @PostMapping("input2")
+//    publicvoidinput2(intn,intm,into,HttpSessionsession)
+//    {
+//      intz=n+m+o;//won'tworkifanyvariableismissing
+//    }
 //
-//@GetMapping("input4")
-//publicintinput4(intn,intm)
-//        {
-//        returnn+m;
-//        }
+//    @GetMapping("input4")
+//    publicintinput4(intn,intm)
+//    {
+//        return n + m;
+//    }
 //
-//
-//@PostMapping("input3")
-//publicModelAndViewinput3(@RequestParam("num")intn)
-//        {
-//        ModelAndViewmv=newModelAndView();
+//    @PostMapping("input3")
+//    publicModelAndViewinput3(@RequestParam("num")intn)
+//    {
+//        ModelAndViewmv = newModelAndView();
 //        mv.addObject("num",n);
 //        mv.setViewName("Home");
-//        returnmv;
-//        }
-    //@PostMapping("Strings")
-    //publicvoidstrings(Stringn,Stringm,HttpSessionsession)
-    //{
-    //LcsController(n,m);
-    //}
+//        return mv;
+//    }
+//
+//    @PostMapping("Strings")
+//    publicvoidstrings(Stringn,Stringm,HttpSessionsession)
+//    {
+//        LcsController(n,m);
+//    }
 }
-
-
-
-
