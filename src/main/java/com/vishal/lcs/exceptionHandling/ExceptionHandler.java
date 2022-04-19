b47c4ee953911b9545fab9d1b406286d07e75eda
Vishal Jain <vishalj.svp2000@gmail.com>
2022-04-19 16:28:37 +0530

    Added ExceptionHandler

---
+++ src/main/java/com/vishal/lcs/exceptionHandling/ExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.vishal.lcs.exceptionHandling;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ExceptionHandler
+{
+//    @ExceptionHandler(value = )
+//
+//    public ResponseEntity<Object> exception(ProductNotfoundException exception)
+//    {
+//        return new ResponseEntity<>("Product not found", HttpStatus.NOT_FOUND);
+//    }
+}
