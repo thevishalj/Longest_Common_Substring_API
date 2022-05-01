162f439e9cf2c110f0221f8c585307430ef8c883
Vishal Jain <vishalj.svp2000@gmail.com>
2022-05-01 17:51:31 +0530

    Added Calulator and LcsServiceTest

--- src/test/java/com/vishal/lcs/LcsApplicationApplicationTests.java
+++ src/test/java/com/vishal/lcs/LcsApplicationApplicationTests.java
@@ -2,12 +2,47 @@ package com.vishal.lcs;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-@SpringBootTest
-class LcsApplicationApplicationTests {
+//@SpringBootTest
+class LcsApplicationApplicationTests
+{
+//    private LcsServiceTest lcsServiceTest;
+//
+//    @Test
+//    void output()
+//    {
+//        String Expected = "Longest common Substring is: [vishal] \nAnd length of LCS is: [6]";
+//        List<String> l1 = new ArrayList<String>();
+//        l1.add("vishaljalsdkfno");
+//        l1.add("vishaljain");
+//        String ActualOutput = lcsServiceTest.LCSprint(l1);
+//        assertThat(ActualOutput).isEqualTo(Expected);
+//    }
 
+    Calculator underTest = new Calculator();
     @Test
-    void contextLoads() {
+    void itShouldAddNumbers()
+    {
+        int numberOne = 20;
+        int numberTwo = 30;
+
+        int result = underTest.add(numberOne, numberTwo);
+
+        int expected = 50;
+        assertThat(result).isEqualTo(expected);
+    }
+}
+
+class Calculator {
+
+    public int add(int a, int b)
+    {
+        return a + b;
     }
 
+    public int doMul(int a, int b)
+    {
+        return a * b;
+    }
 }
